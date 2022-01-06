94b2cb9988ad814592b724fcd2f6850bf9e90a7a
Svyatoslav Lazarev <silazare.nsk@gmail.com>
2022-01-06 15:00:13 +0700

    add: solutuions for chapters 1.2 and 1.3

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,10 @@
+#!/usr/bin/python3
 # bounce.py
 #
 # Exercise 1.5
+
+if __name__ == '__main__':
+    height = 100
+    for i in range(1,11):
+        height = height * 3 / 5
+        print(round(height,4))
