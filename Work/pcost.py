b8c62b09fc1c434b274496bbbfeb15d55d6548e6
Svyatoslav Lazarev <silazare.nsk@gmail.com>
2022-01-18 22:43:53 +0700

    add: exercise 1.27 solution

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,18 @@
+#!/usr/bin/python3
+#
 # pcost.py
 #
 # Exercise 1.27
+
+total_cost = 0
+
+with open('Data/portfolio.csv', 'rt') as f:
+    headers = next(f) # remove 1st header line
+    for line in f:
+        row = line.split(',')
+        print(row)
+        cost = int(row[1]) * float(row[2].strip())
+        total_cost = total_cost + cost
+
+print('====')
+print(f'Total cost: {total_cost:0.2f}')
