d186eafa52f558188f53886d40a19dbb3f989d89
Svyatoslav Lazarev <silazare.nsk@gmail.com>
2022-01-20 15:40:40 +0700

    add: exercise 1.27 functions

--- Work/pcost.py
+++ Work/pcost.py
@@ -4,15 +4,55 @@
 #
 # Exercise 1.27
 
-total_cost = 0
+import csv
+import sys
 
-with open('Data/portfolio.csv', 'rt') as f:
-    headers = next(f) # remove 1st header line
-    for line in f:
-        row = line.split(',')
-        print(row)
-        cost = int(row[1]) * float(row[2].strip())
-        total_cost = total_cost + cost
+def portfolio_cost(filename):
+    """
+    :param filename:
+    :return:
+    """
+    total = 0
+    with open(filename, 'rt') as f:
+        headers = next(f)  # remove 1st header line
+        for line in f:
+            row = line.split(',')
+            # print(row)
+            try:
+                cost = int(row[1]) * float(row[2].strip())
+                total = total + cost
+            except ValueError:
+                print("WARNING: Couldn't parse a line!", row)
+                pass
+    return total
 
-print('====')
-print(f'Total cost: {total_cost:0.2f}')
+
+def portfolio_cost_csv(filename):
+    """
+    Parse a file using CSV module
+    :param filename:
+    :return:
+    """
+    total = 0
+    with open(filename, 'rt') as f:
+        rows = csv.reader(f)
+        headers = next(rows)  # remove 1st header line
+        for row in rows:
+            # print(row)
+            try:
+                cost = int(row[1]) * float(row[2])
+                total = total + cost
+            except ValueError:
+                print("WARNING: Couldn't parse a line!", row)
+                pass
+    return total
+
+
+if __name__ == '__main__':
+    if len(sys.argv) == 2:
+        file = sys.argv[1]
+    else:
+        file = 'Data/portfolio.csv'
+    total_cost = portfolio_cost_csv(file)
+    #total_cost = portfolio_cost(file)
+    print(f'Total cost: {total_cost:0.2f}')
